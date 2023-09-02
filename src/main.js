ed2aed7d592e8f845fe4c9e6c2e35219ee189e71
IngridKao <ingrid7613@gmail.com>
2023-09-02 16:24:57 +0800

    新增iview的Carousel

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import 'view-ui-plus/dist/styles/viewuiplus.css'
 
 // import "./assets/scss/reset.scss"
 
+// 原始
+// createApp(App).use(store).use(router).mount('#app')
+// 加入ViewUIPlus
+// createApp(App).use(store).use(router).use(ViewUIPlus).mount('#app')
+
+// 先宣告
 const vueApp = createApp(App)
 vueApp.use(store)
     .use(router)
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="product">
-    <h1>This is an product page</h1>
+    <h1>
+      {{ text }}
+    </h1>
+    <button @click="callName">123</button>
   </div>
+  <Carousel v-model="value" loop>
+      <CarouselItem>
+          <div class="demo-carousel">1</div>
+      </CarouselItem>
+      <CarouselItem>
+          <div class="demo-carousel">2</div>
+      </CarouselItem>
+      <CarouselItem>
+          <div class="demo-carousel">3</div>
+      </CarouselItem>
+      <CarouselItem>
+          <div class="demo-carousel">4</div>
+      </CarouselItem>
+  </Carousel>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+          value: 0,
+          text: 'This is an product page'
+      }
+    },
+    methods: {
+      callName(){
+        this.text = 'eeeee'
+      }
+    },
+  }
+</script>
+<style>
+.demo-carousel{
+  width: 100%;
+  height: 10rem;
+  background-color: #ddd;
+}
+</style>
